cb8ecf01ec113f5f37cb573bc742e99de96c15b7
Yusuf Isaacs <m.yusuf.isaacs@gmail.com>
2024-10-26 19:07:52 +0200

    Implement UserDataSource

--- src/main/kotlin/com/example/di/KoinModule.kt
+++ src/main/kotlin/com/example/di/KoinModule.kt
@@ -1,6 +1,8 @@
 package com.example.di
 
 import com.example.common.Constants
+import com.example.data.UserDataSourceImpl
+import com.example.domain.repository.UserDataSource
 import org.koin.dsl.module
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
@@ -10,5 +12,9 @@ val koinModule = module {
         KMongo.createClient()
             .coroutine
             .getDatabase(Constants.DATABASE_NAME)
+    }
+
+    single<UserDataSource> {
+        UserDataSourceImpl(get())
     }
 }
